27e40a7634680f8c7b8615df93a5db896254c1b4
Natasha <n.crain@outlook.com>
2022-04-11 18:56:27 +0100

    continued styling -changed h1 logo to img src logo -added arrows to gallery -changed colors, font sizes, font weights, background color etc etc of div class 'card' to follow design

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -3,7 +3,17 @@
 const overlay = document.getElementById('overlay');
 const hamMenu = document.getElementById('hamMenu');
 const closeMenu = document.getElementById('closeMenu');
+const menu = document.querySelector('.nav-toggle-label');
+
+const toggleIcon = () => {
+    if(hamMenu.src == 'static/images/icon-menu.svg'){
+        hamMenu.src =' static/images/icon-close.svg';
+    } else {
+        hamMenu.src == 'static/images/icon-menu.svg'
+    }
+};
+
 hamMenu.addEventListener('click', () => {
     overlay.classList.toggle('visible');
+    toggleIcon();
 });
-
