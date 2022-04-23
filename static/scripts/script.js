811e36153527535a8429f55491f49b2890889795
Natasha <n.crain@outlook.com>
2022-04-23 17:03:02 +0100

    created js for updating cart - created increment and decrement counter 	- bug with decrement: it goes to -1 and then back to 0 when 	  clicking on minus button 	- added max and min values for adding to cart, but they are also 	  a little weird, but these two issues might be related - created cost count - created click eventlistener to the add to cart button 	- what to change innerhtml of button to change to 'update cart' 	  when cart is >= 1 - cart modal changes from 'cart is empty' to diplaying item, prices etc 	- need to do this in reverse i.e. when cart is back to zero, the 	  modal display 'cart is empty' again - need to add function where all items are removed from cart when bin   button is clicked

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -17,6 +17,78 @@ document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click', 
     overlay.classList.remove('active');
 }))
 
+let products = [
+    {
+        name: 'Sneakers',
+        tag: 'fallsneakers',
+        price: 125,
+        inCart: 0
+    }
+]
 
+let cartBtn = document.getElementById('addCartBtn');
+let plusItems = document.getElementById('plus');
+let minusItems = document.getElementById('minus');
+let unitNumber = document.getElementById('cartNum');
+let cartItems = document.getElementById('cartCount');
+let itemTotal = document.getElementById('itemTotal');
+let priceTotal = document.getElementById('priceTotal');
+let cartEmpty = document.getElementById('cartEmpty');
+let cartContainer = document.getElementById('cartItems');
 
+let count = 0
+let cost = 0
 
+const addUnit = () => {
+    count++;
+    cost += 125 ;
+    unitNumber.innerHTML = count;
+    count = Math.min(count, 9);
+};
+
+const minusUnit = () => {
+    count--;
+    cost -= 125;
+    unitNumber.innerHTML = count;
+    count = Math.max(count, 1);
+};
+
+plusItems.addEventListener('click', () => {
+    addUnit();
+});
+
+minusItems.addEventListener('click', () => {
+    minusUnit();
+});
+
+const itemUpdate = () => {
+    itemTotal.innerHTML = count;
+    priceTotal.innerHTML = `$ ${cost}.00`;
+
+};
+
+cartBtn.addEventListener('click', () => {
+    addToCart();
+    itemUpdate();
+    unhideCart();
+    //updateBtn();
+});
+
+const addToCart = () => {
+    console.log(typeof count);
+    //unitNumber = parseInt(unitNumber);
+    cartItems.innerHTML = count;
+    //console.log(typeof count);
+};
+
+const unhideCart = () => {
+    if(count >= 1) {
+        cartContainer.style.display = 'flex';
+        cartEmpty.style.display = 'none';
+    }
+};
+/*
+const updateBtn = () => {
+    let changeBTN = document.getElementById('updateBTN');
+    changeBTN.innerHTML = 'Update Cart';
+};*/
